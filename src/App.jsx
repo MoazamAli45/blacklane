57524e182860ecd79355dd1858b38ff306c9bf0b
MoazamAli45 <21-se-48@students.uettaxila.edu.pk>
2024-03-10 03:48:32 +0500

    Add appstore and googleplay images, and SignatureDownload component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Hero from "./components/Hero";
 import LongDistance from "./components/LongDistance";
 import Navbar from "./components/Navbar";
 import Routes from "./components/Routes";
+import Signature from "./components/Signature";
+import SignatureDownload from "./components/SignatureDownload";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
       <LongDistance />
       <Routes />
       <Faq />
+      <Signature />
+      <SignatureDownload />
     </>
   );
 }
--- src/components/Download/index.jsx
+++ src/components/Download/index.jsx
@@ -1,6 +1,8 @@
 import Wrapper from "../shared/Wrapper";
 import Qr from "../../assets/Hero/qr.svg";
 // import downloadFromApple from "../../assets/Hero/download-from-apple.png";
+import downloadFromApple from "../../assets/Hero/appstore.png";
+import downloadFromGoogle from "../../assets/Hero/googleplay.png";
 
 const Download = () => {
   return (
@@ -14,12 +16,17 @@ const Download = () => {
           you&apos;re on the go.
         </p>
         <div className="flex gap-4 mt-8 justify-center md:justify-start">
-          <img src={Qr} alt="qr code" />
-          <span
-            // src={downloadFromApple}
-            // alt="download from apple"
-            className="downloadApple"
-          ></span>
+          <img src={Qr} alt="qr code" className="hidden md:block" />
+          <img
+            src={downloadFromApple}
+            alt="download from apple"
+            className="w-[119px] h-[34px] object-cover rounded-md border-[1px] border-solid border-white self-end"
+          />
+          <img
+            src={downloadFromGoogle}
+            alt="download from google"
+            className="w-[119px] h-[34px] object-cover rounded-md border-[1px] border-solid border-white self-end"
+          />
         </div>
       </Wrapper>
     </div>
